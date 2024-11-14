72261f921076a9daa929de62bbacadc64411a151
Fanuk <kirillsvirid2006@gmail.com>
2024-11-14 15:09:26 +0700

    optimazed horizontal scroll

--- app/components/HorizontalAnimation/HorizontalAnimation.tsx
+++ app/components/HorizontalAnimation/HorizontalAnimation.tsx
@@ -1,13 +1,16 @@
 "use client"
-import React from 'react'
+import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export const HorizontalAnimation = () => {
+    const contentRef = useRef<HTMLUListElement>(null)
     useGSAP(() => {
+        if (!contentRef.current) return;
+        const contentWidth = contentRef.current.scrollWidth
         gsap.registerPlugin(ScrollTrigger)
-        gsap.to('#content', {
+        gsap.to(contentRef.current, {
             scrollTrigger: {
                 trigger: '#container',
                 start: 'top top',
@@ -15,14 +18,14 @@ export const HorizontalAnimation = () => {
                 pin: true,
                 scrub: 1
             },
-            x: '-80%',
+            x: -contentWidth / 2,
             ease: 'none'
         })
     })
   return (
     <div id='container' className='w-screen h-screen overflow-hidden mt-10 mb-10'>
-        <ul id='content' className='w-full h-full flex items-center gap-4'>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 1</li>
+        <ul ref={contentRef} className='w-full h-full flex items-center gap-4'>
+            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0 ml-5'>Item 1</li>
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 2</li>
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 3</li>
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 4</li>
@@ -31,7 +34,7 @@ export const HorizontalAnimation = () => {
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 7</li>
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 8</li>
             <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 9</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 10</li>
+            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0 mr-5'>Item 10</li>
         </ul>
     </div>
   )
