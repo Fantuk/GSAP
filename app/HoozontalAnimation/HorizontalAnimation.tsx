70163b4ea4068d8b47bba7aef913360eacd2160f
Fanuk <kirillsvirid2006@gmail.com>
2024-11-14 19:38:35 +0700

    optimized infinity scroll

--- app/HoozontalAnimation/HorizontalAnimation.tsx
+++ app/HoozontalAnimation/HorizontalAnimation.tsx
@@ -1,68 +1,37 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 export const HorizontalAnimation = () => {
+  const contentRef = useRef<HTMLUListElement>(null);
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   useGSAP(() => {
-    gsap.from("#right", {
-      xPercent: -52.5,
-      ease: "none",
-      repeat: -1,
-      duration: 10,
-    });
-    gsap.to("#left", {
-        xPercent: -52.5,
+    if (!contentRef.current) return;
+    const contentWidth = contentRef.current.scrollWidth / 2;
+    const itemWidth = contentWidth / data.length;
+
+    gsap.fromTo(
+      contentRef.current,
+      {
+        x: -contentWidth,
+      },
+      {
+        x: itemWidth / 2,
         ease: "none",
         repeat: -1,
         duration: 10,
-      });
+      }
+    );
   }, []);
+
   return (
-    <div className="w-full overflow-hidden flex flex-col gap-8">
-      <ul id="right" className="flex gap-2 justify-between w-[200%]">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-      </ul>
-      <ul id="left" className="flex gap-2 justify-between w-[200%]">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
+    <div className="w-screen overflow-hidden flex flex-col gap-8">
+      <ul ref={contentRef} className="flex gap-2 justify-between w-[200%]">
+        {[...data, ...data].map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
       </ul>
     </div>
   );
