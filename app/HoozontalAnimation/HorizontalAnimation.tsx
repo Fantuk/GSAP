c9ac82cb1a6130791f543d5acfb946742a368e15
Fanuk <kirillsvirid2006@gmail.com>
2024-11-19 21:59:08 +0700

    refactor logic

--- app/HoozontalAnimation/HorizontalAnimation.tsx
+++ app/HoozontalAnimation/HorizontalAnimation.tsx
@@ -1,36 +1,52 @@
 "use client";
 import React, { useRef } from "react";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export const HorizontalAnimation = () => {
   const contentRef = useRef<HTMLUListElement>(null);
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const items = useRef<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
   useGSAP(() => {
     if (!contentRef.current) return;
-    const contentWidth = contentRef.current.scrollWidth / 2;
-    const itemWidth = contentWidth / data.length;
 
-    gsap.fromTo(
+    const contentWidth = contentRef.current.scrollWidth;
+    const itemWidth = contentWidth / items.current.length;
+
+    const shiftArray = () => {
+      const content = contentRef.current;
+      if (content && content.children.length > 0) {
+        const lastChild = content.lastElementChild;
+        if (lastChild) {
+          content.insertBefore(lastChild, content.firstChild);
+        }
+      }
+    };
+
+    const animation = gsap.fromTo(
       contentRef.current,
+      { x: -itemWidth },
       {
-        x: -contentWidth,
-      },
-      {
-        x: itemWidth / 2,
+        x: 0,
         ease: "none",
         repeat: -1,
-        duration: 10,
+        duration: 3,
+        onRepeat: shiftArray,
       }
     );
+
+    return () => {
+      animation.kill();
+    };
   }, []);
 
   return (
     <div className="w-screen overflow-hidden flex flex-col gap-8">
-      <ul ref={contentRef} className="flex gap-2 justify-between w-[200%]">
-        {[...data, ...data].map((el, index) => (
-          <li key={index}>{el}</li>
+      <ul ref={contentRef} className="flex gap-2 w-[200%]">
+        {items.current.map((el, index) => (
+          <li className="w-[100%]" key={index}>
+            {el}
+          </li>
         ))}
       </ul>
     </div>
