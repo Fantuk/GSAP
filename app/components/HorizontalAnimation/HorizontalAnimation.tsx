a701df8755c322a7ce56228b66e84f717db744f2
Fanuk <kirillsvirid2006@gmail.com>
2024-11-21 16:55:57 +0700

    optimazed horizontal scroll

--- app/components/HorizontalAnimation/HorizontalAnimation.tsx
+++ app/components/HorizontalAnimation/HorizontalAnimation.tsx
@@ -1,48 +1,88 @@
-"use client"
-import React, { useRef } from 'react'
-import { useGSAP } from '@gsap/react'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const HorizontalAnimation = () => {
-    const contentRef = useRef<HTMLUListElement>(null)
-    useGSAP(() => {
-        if (!contentRef.current) return;
-        const contentGap = contentRef.current 
-            ? parseInt(window.getComputedStyle(contentRef.current).gap) : 0;
+  const contentRef = useRef<HTMLUListElement>(null);
+  const [visibleItems, setVisibleItems] = useState<number[]>([]);
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  useGSAP(() => {
+    if (!contentRef.current) return;
+    const contentGap = contentRef.current
+      ? parseInt(window.getComputedStyle(contentRef.current).gap)
+      : 0;
 
-        const scrollbarWidth = (window.innerWidth - document.documentElement.clientWidth) 
-        const contentWidth = contentRef.current.scrollWidth - contentRef.current.offsetWidth + scrollbarWidth
-        gsap.registerPlugin(ScrollTrigger)
-        gsap.fromTo(contentRef.current, {
-            x: contentGap
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+    const contentWidth =
+      contentRef.current.scrollWidth -
+      contentRef.current.offsetWidth +
+      scrollbarWidth;
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      contentRef.current,
+      {
+        x: contentGap,
+      },
+      {
+        scrollTrigger: {
+          trigger: "#container",
+          start: "top top",
+          end: "+=100%",
+          pin: true,
+          scrub: 1,
         },
-         {
-            scrollTrigger: {
-                trigger: '#container',
-                start: 'top top',
-                end: '+=100%',
-                pin: true,
-                scrub: 1
-            },
-            x: -contentWidth - contentGap,
-            ease: 'none'
-        })
-    }, [])
+        x: -contentWidth - contentGap,
+        ease: "none",
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.map((entry) => {
+        if (entry.isIntersecting) {
+          const index = parseInt(entry.target.getAttribute("data-index") || "");
+          if (!visibleItems.includes(index)) {
+            setVisibleItems((prev) => [...prev, index]);
+          }
+        }
+      });
+    }, {
+        root: null,
+        threshold: 0.1,
+    });
+
+    if (!contentRef.current) return;
+    const items = contentRef.current.children;
+    if (items) {
+      Array.from(items).map((item) => observer.observe(item));
+    }
+
+    return () => observer.disconnect();
+  }, [visibleItems]);
   return (
-    <div id='container' className='w-screen h-screen overflow-hidden mt-10 mb-10'>
-        <ul ref={contentRef} className='w-full h-full flex items-center gap-4'>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 1</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 2</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 3</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 4</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 5</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 6</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 7</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 8</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 9</li>
-            <li className='bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0'>Item 10</li>
-        </ul>
+    <div
+      id="container"
+      className="w-screen h-screen overflow-hidden mt-10 mb-10"
+    >
+      <ul ref={contentRef} className="w-full h-full flex items-center gap-4">
+        {data.map((item, index) => (
+          <li
+            className="bg-red-500 h-[300px] w-[200px] flex items-center justify-center shrink-0"
+            key={index}
+            data-index={index}
+            style={{
+              opacity: visibleItems.includes(index) ? 1 : 0,
+              transition: "opacity 0.5s",
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
