3a583e94c44491a32a3f2a5ddbfae19c2988773f
Fanuk <kirillsvirid2006@gmail.com>
2024-11-27 00:27:46 +0700

    youtube version

---
+++ app/data/videoCarousel.tsx
@@ -0,0 +1,28 @@
+interface IHighlight {
+    id: number;
+    videoPath: string;
+    videoDuration: number;
+}
+
+export const hightlightsSlides: IHighlight[] = [
+    {
+        id: 1,
+        videoPath: "video.mp4",
+        videoDuration: 5
+    },
+    {
+        id: 2,
+        videoPath: "video.mp4",
+        videoDuration: 5
+    },
+    {
+        id: 3,
+        videoPath: "video.mp4",
+        videoDuration: 5
+    },
+    {
+        id: 4,
+        videoPath: "video.mp4",
+        videoDuration: 5
+    },
+]
