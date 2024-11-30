1c535818c0ebc401bf9d54e0e48f4e39c5580241
Fanuk <kirillsvirid2006@gmail.com>
2024-11-30 18:58:03 +0700

    optimazed

--- app/components/VideoCarousel/VideoCarousel.tsx
+++ app/components/VideoCarousel/VideoCarousel.tsx
@@ -4,10 +4,12 @@ import React, { useState, useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { VideoItem } from "./VideoItem";
 
 export const VideoCarousel = () => {
   const videoRefs = useRef<HTMLVideoElement[]>([]);
-  const paginationRefs = useRef<HTMLDivElement[]>([]);
+  const sliderRefs = useRef<HTMLLIElement[]>([]);
+  const paginationRefs = useRef<HTMLLIElement[]>([]);
   const progressbarRefs = useRef<HTMLSpanElement[]>([]);
   const [video, setVideo] = useState({
     isPlaying: false,
@@ -19,39 +21,27 @@ export const VideoCarousel = () => {
 
   const { isPlaying, isVideoEnd, videoId, isLastVideo, startPlay } = video;
 
-  const handleVideoProcess = (id: number, type: string) => {
-    switch (type) {
-      case "play":
-        setVideo((prev) => ({ ...prev, isPlaying: !prev.isPlaying }));
-        break;
-
-      case "end":
-        setVideo((prev) => ({
-          ...prev,
-          isVideoEnd: true,
-          videoId: id + 1,
-        }));
-        break;
-
-      case "reset":
-        setVideo((prev) => ({ ...prev, isLastVideo: false, videoId: 0 }));
-        break;
-
-      case "pause":
-        setVideo((prev) => ({ ...prev, isPlaying: !prev.isPlaying }));
-        break;
-
-      case "lastVideo":
-        setVideo((prev) => ({ ...prev, isLastVideo: true }));
-        break;
-
-      default:
-        return video;
-    }
+  const handleVideoProcess = (
+    id: number,
+    type: "play" | "pause" | "end" | "reset" | "lastVideo"
+  ) => {
+    setVideo((prev) => {
+      const updates = {
+        play: { isPlaying: true },
+        pause: { isPlaying: false },
+        end: { isVideoEnd: true, videoId: id + 1 },
+        reset: { isLastVideo: false, videoId: 0 },
+        lastVideo: { isLastVideo: true },
+      };
+      return { ...prev, ...updates[type] };
+    });
   };
 
+  // Используем GSAP для анимации контейнера видео
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
+    if (!videoRefs.current[videoId]) return;
+
     gsap.to("#slider", {
       x: `-${videoId * 100}%`,
       duration: 1,
@@ -69,6 +59,7 @@ export const VideoCarousel = () => {
     });
   }, [videoId, isVideoEnd]);
 
+  // Эффект управления текущим видео и прогрессом
   useEffect(() => {
     const videoElement = videoRefs.current[videoId];
     const progressbar = progressbarRefs.current[videoId];
@@ -79,12 +70,23 @@ export const VideoCarousel = () => {
     const updateProgress = () => {
       const progress =
         (videoElement.currentTime / videos[videoId].videoDuration) * 100;
-      gsap.to(pagination, { width: "10vw" });
       gsap.to(progressbar, {
         width: `${progress + 5}%`,
         backgroundColor: "green",
       });
+      gsap.to(pagination, { width: "10vw" });
     };
+
+    const handleVideoEnd = () => {
+      setTimeout(() => {
+        handleVideoProcess(
+          videoId,
+          videoId === videos.length - 1 ? "lastVideo" : "end"
+        );
+      }, 300);
+    };
+
+    videoElement.onended = handleVideoEnd;
 
     if (isPlaying) {
       videoElement.play();
@@ -100,64 +102,44 @@ export const VideoCarousel = () => {
     };
   }, [videoId, isPlaying]);
 
+  // Эффект синхронизации всех прогресс-баров и видео
   useEffect(() => {
-    progressbarRefs.current.map((progressBar) => {
+    progressbarRefs.current.forEach((progressBar) => {
       if (progressBar && isPlaying) {
         gsap.to(progressBar, { width: "0%", backgroundColor: "transparent" });
       }
     });
 
-    paginationRefs.current.map((pagination) => {
+    paginationRefs.current.forEach((pagination) => {
       if (pagination && isPlaying) {
         gsap.to(pagination, { width: "12px" });
       }
     });
 
-    videoRefs.current.map((videoEl, index) => {
-      if (index !== videoId && videoEl) {
-        videoEl.pause();
-        videoEl.currentTime = 0;
-      }
-    });
-
-    if (videoRefs.current[videoId] && startPlay) {
-      isPlaying
-        ? videoRefs.current[videoId].play()
-        : videoRefs.current[videoId].pause();
-    }
   }, [videoId, isPlaying, startPlay]);
 
   return (
     <section className="flex flex-col justify-center gap-5 w-full">
-      <div className="flex items-center">
+      <ul className="flex items-center list-none">
         {videos.map((video, index) => (
-          <div key={index} className="sm:pr-20 pr-10 shrink-0" id="slider">
-            <div className="sm:w-[70vw] w-[88vw] md:h-[70vh] sm:h-[50vh] h-[35vh]">
-              <div className="w-full h-full flex items-center justify-center rounded-3xl overflow-hidden bg-black">
-                <video
-                  src={video.videoPath}
-                  playsInline={true}
-                  muted
-                  preload="auto"
-                  className="w-full h-full"
-                  id="video"
-                  ref={(ref) => {
-                    if (ref) videoRefs.current[index] = ref;
-                  }}
-                  onPlay={() =>
-                    setVideo((prev) => ({ ...prev, isPlaying: true }))
-                  }
-                ></video>
-              </div>
-            </div>
-          </div>
+          <VideoItem
+            key={index}
+            videoPath={video.videoPath}
+            videoAnimationId="video"
+            sliderAnimationId="slider"
+            index={index}
+            videoRefs={videoRefs}
+            sliderRefs={sliderRefs}
+            currentVideoId={videoId}
+            setVideo={setVideo}
+          />
         ))}
-      </div>
+      </ul>
 
       <div className="flex items-center justify-center gap-5 relative">
-        <div className="flex items-center justify-center py-5 px-7 bg-black backdrop-blur rounded-full">
-          {videoRefs.current.map((_, index) => (
-            <div
+        <ul className="flex items-center justify-center py-5 px-7 bg-black backdrop-blur rounded-full list-none">
+          {sliderRefs.current.map((_, index) => (
+            <li
               className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer overflow-hidden"
               key={index}
               ref={(ref) => {
@@ -177,9 +159,9 @@ export const VideoCarousel = () => {
                   if (ref) progressbarRefs.current[index] = ref;
                 }}
               ></span>
-            </div>
+            </li>
           ))}
-        </div>
+        </ul>
         <button
           className="bg-black py-5 px-6 rounded-full text-white"
           onClick={() => {
